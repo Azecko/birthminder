d5e830777a1219ba03e9f509575737d7b096b4e3
Azecko <sami.panseri@epfl.ch>
2022-06-17 10:35:13 +0200

    [feat] UI improvement
    UI was really bad on mobile. It's now a bit better.
    Also removed import that were not used.

--- birthminder/src/pages/insert.tsx
+++ birthminder/src/pages/insert.tsx
@@ -1,9 +1,8 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonButton, IonDatetime, IonInput, IonItem, IonAlert, IonLabel, IonCard } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonDatetime, IonInput, IonItem, IonAlert, IonLabel } from '@ionic/react';
 import { format, parseISO } from 'date-fns';
 import { useState } from 'react';
 import registerBirthday from '../components/registerBirthday';
 import deleteBirthday from '../components/deleteBirthday';
-import getIndex from '../components/getIndex';
 import isBirthday from '../components/isBirthday';
 
 const Insert: React.FC = () => {
@@ -42,15 +41,13 @@ const Insert: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         {alert('Alert', '', 'Please enter a valid name and a valid date.')}
-        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent:'center', height: '100vh', gap: '30px'}}>
-          <IonItem>
-            <IonLabel>Name</IonLabel>
-            <IonInput placeholder="Alexandre" onIonChange={e => setSelectedName(e.detail.value!)}></IonInput>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent:'center', marginTop: '1vh', gap: '20px'}}>
+          <IonItem color="light">
+            <IonLabel>Enter a name</IonLabel>
+            <IonInput placeholder="Alexandre" color="dark" onIonChange={e => setSelectedName(e.detail.value!)}></IonInput>
           </IonItem>
-          <IonCard>
-            <IonTitle>Date of the birthday</IonTitle>
+          <IonLabel>Date of the birthday</IonLabel>
             <IonDatetime presentation="date" onIonChange={e => setSelectedDate(formatDate(e.detail.value!))}></IonDatetime>
-          </IonCard>
           <IonButton color="primary" onClick={e => registerBirthday(selectedDate, selectedName) || setShowAlert(true)}>Insert</IonButton>
           {
             Object.keys(allBirthdays).length > 1 ? (
